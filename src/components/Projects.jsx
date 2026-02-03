a4204fa48f77b77f780c5fb34aaa19c34e846fa2
AdamPandey <N00234646@iadt.ie>
2026-02-03 09:00:00 +0000

    feat: define project data array for cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,7 @@
-import { Text } from '@react-three/drei'
 export const projectData = [
-  { title: "Project 1", description: "Description 1" },
-  { title: "Project 2", description: "Description 2" }
+  { title: "App 1", description: "Web" },
+  { title: "App 2", description: "Web" }
 ]
 export default function Projects() {
-  return (
-    <group>
-      {projectData.map((p, i) => (
-        <mesh key={i} position={[0, -i * 5, 0]}>
-          <planeGeometry args={[4, 2.25]} />
-          <meshBasicMaterial color="#222" />
-        </mesh>
-      ))}
-    </group>
-  )
+  return null
 }
