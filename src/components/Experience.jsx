f434ce5b4c2fd60f2d1426e5870468cacac1830a
AdamPandey <N00234646@iadt.ie>
2026-02-03 07:00:00 +0000

    feat: integrate scroll controls for navigation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,13 @@
-import { Sparkles } from '@react-three/drei'
+import { ScrollControls, Sparkles, Scroll } from '@react-three/drei'
+import Projects from './Projects'
 export default function Experience() {
   return (
     <>
       <color attach="background" args={['#09090b']} />
-      <Sparkles count={100} scale={10} size={1} />
+      <Sparkles count={300} scale={20} size={1.5} />
+      <ScrollControls pages={8} damping={0.1}>
+        <Scroll><Projects /></Scroll>
+      </ScrollControls>
     </>
   )
 }
