b38cae2c371779ad8b98d590ebe1ea9967f7fe19
AdamPandey <N00234646@iadt.ie>
2026-02-03 11:00:00 +0000

    feat: implement initial sparkles particle system

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,9 @@
+import { Sparkles } from '@react-three/drei'
 export default function Experience() {
-  return <color attach="background" args={['#050505']} />
+  return (
+    <>
+      <color attach="background" args={['#09090b']} />
+      <Sparkles count={100} scale={10} size={1} />
+    </>
+  )
 }
